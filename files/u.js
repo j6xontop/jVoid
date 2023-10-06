df8922fdea3051bc27c837300ff3e16d84a02ab3
talnrvwboxf <147206337+talnrvwboxf@users.noreply.github.com>
2023-10-06 16:40:25 -0400

    FUCK YOU SKIDS NIGGA KYS NOW

---
+++ files/u.js
@@ -0,0 +1,93 @@
+------------ INFORMATION ------------
+Name: Darian Mohaseb
+Nationality: Turkish, Egyptian
+
+Parent/Guardian-1
+Sam Mohaseb (Father): sam.mohaseb@gmail.com
+Parent/Guardian-2
+Niloufar Mohaseb (Mother): niloufar.rad@gmail.com
+Registered Parent Account-1
+Sam Mohaseb (Father) : sam.mohaseb@gmail.com
+Registered Parent Account-2
+Niloufar Mohaseb (Mother) : niloufar.rad@gmail.com
+
+------------ OTHER ------------
+Fathers Website: https://smohaseb.com/
+Fathers amazing face: https://media.discordapp.net/attachments/1158886109338677258/1158901122602389596/1674859008137.png?ex=651dee46&is=651c9cc6&hm=36d95396f6869f852af1b2bad76c39bfdbd1c1c7c51520332e581d0e127ba7f5&=&width=625&height=625
+Mothers number: (904) 230-7877
+More info on mother: https://www.floridaresidentsdirectory.com/person/117989973/mohaseb-niloufar?amp
+Even more info on mother: https://www.officialusa.com/names/Niloufar-Mohaseb/
+More info on father: https://www.officialusa.com/names/Sam-Mohaseb/
+
+------------ LOCATION ------------
+ADDRESS: 2885 Everholly Lane
+IP: 38.85.144.114
+Hostname: Unknown
+Country Code: US (USA)
+Country: United States
+Region: Florida
+City: Jacksonville
+Postal Code / zip: 32207
+ISP: IQ Fiber
+Organization: IQ-FIBER
+ASN: AS400427
+Continent: North America (NA)
+Timezone: America/New_York (Tue, 03 Oct 2023 19:05:41 -0400)
+https://maps.google.com/?ll=30.2884,-81.6361
+
+------------ CLEVER LOGIN ------------
+https://clever.com/in/rcsa
+
+user: s20099394@rivercityscience.org
+pass: D26m9394
+
+------------ SCHOOL ------------
+School name: RCSA - MANDARIN
+School number: +19048558010
+Clubs: his dumb ass isn't in any
+Hours:8:00 a.m. – 2:45 p.m. 
+Schedule:7:30- 8:00  
+Bellwork/Reading/Activity  
+8:00-8:30 (30)  
+1ST PERIOD  
+8:33-9:03 (30)  
+2ND PERIOD  
+9:06-9:36 (30)  
+3RD PERIOD  
+9:39-10:14 (35)  
+4TH PERIOD 
+10:17-10:52 (35)  
+5TH PERIOD  
+10:55-11:30 (35)  
+6TH PERIOD  
+11:33-12:45  
+7TH PERIOD  
+First Lunch- 11:45-12:15  
+Second Lunch- 12:25-12:55  
+1:00: 7th period Dismissal @@        `@@@`  @@                 @@@@                                          
+                                          `@@@.                 @#   ;@@#        @@@`            @@@`       :@@#   .@#                 @@@@                                          
+                                           @@@:                 @@    ,@@@:       @@@           #@@;      .@@@;    @@                  @@@,                                          
+                                           @@@@                 ;@;     +@@@;     ;@@#         .@@@     ,#@@@`     @@                  @@@                                           
+                                           @@@@                  @@      +@@@@#:   @@@`        @@@   `'@@@@+      '@:                 .@@@                                           
+                                           '@@@                  @@;      .,'@@@@@'+@@@       ;@@@;#@@@@#...      @@                  +@@@                                           
+                                           `@@@#`                :@@`     `   ,+@@@@@@@.      @@@@@@@#:   :      @@@                 '@@@@                                           
+                                            @@@@@#                @@@            `'#@@@@     ;@@@@;:            +@@;               '@@@@@`                                           
+                                            `@@@@@@'              @@@@            `+ ;@@`    @@@ .:            ;@@@              ,@@@@@@:                                            
+                                              +@@@@@@`            '@@@#    #`         @@@   .@@.         #    ,@'@@             #@@@@@@`                                             
+                                               .@@@@@@'            @@ @+   @@         ;@@   @@@         #@   .@,@@+           .@@@@@@:                                               
+                                                 ;@@@@@#           @@` @'  @@@         @@.  @@`        +@@  `@, @@           '@@@@@@                                                 
+                                                  `@@@@@@          #@@  @' @.@@    @,  '@@ ;@@   @    ;@,@ `@. .@@          #@@@@@,                                                  
+                                                    +@@@@@          @@   #'@:`@+   @@   @@ @@,  @#;  `@# @ @`  @@#         @@@@@@`                                                   
+                                                     ,@@@@@`        @@:   ;@@ ;@.  @`@  #@@@@  .+,@  @#  @#    @@         @@@@@@                                                     
+                                                      `@@@@@        #@@    .@  +@ `@ @`  @@@;  @ `@ @@  ++    ;@@        @@@@@+                                                      
+                                                       `@@@@@        @@         #@ '  @  @@@  ';  @:@         @@'       +@@@@'                                                       
+                                                        `@@@@@       @@@         ##;  +, @'@  @   #@         `@@       :@@@@+                                                        
+                                                         .@@@@;      ,@@          #:   @ @ @ ',   @          @@@       @@@@@                                                         
+                                                          +@@@@       @@#          .   ;`@ ;`@              .@@       @@@@@                                                          
+                                                           @@@@@      :@@`              #'  #               @@@      `@@@@                                                           
+                                                            @@@@   .   @@@                  '              #@@   ;   @@@@#                                                           
+                                                            @@@@#   `   @@@  `                            '@@'   #  `@@@@                                                            
+                                                             @@@@   @   :@@@+;                         `''@@@   ;`  @@@@+                                                            
+                                                             @@@@.  @`   '@@@.                         +@@@@    @   @@@@                                                             
+                                                             .@@@@  @@    ;@@@@`    `            ,    +@@@#    .@  `@@@@                                                             
+                                                              @@@@  `@     `@@@@@:;'#   `       ,.':'@@@@;     @@  '@@@:      
